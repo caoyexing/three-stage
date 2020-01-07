f925ab897c9d977029a3a451e0206216256625c8
caoyexing <1318839214@qq.com>
2020-01-07 12:11:47 +0800

    第一次提交

---
+++ react-lianxi/back-stage/src/pages/Admin/Admin.js
@@ -0,0 +1,10 @@
+import React from 'react-dom'
+import { Fragment } from 'react'
+class Admin extends Comment {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+export default Admin
---
+++ react-lianxi/back-stage/src/pages/Set/Set.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+class Login extends Component{
+  render(){
+    return(
+      <div>Set</div>
+    )
+  }
+}
+export default Login
