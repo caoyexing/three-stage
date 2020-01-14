d456bc72d1a7835e63e3a7896d124713c9c197d4
caoyexing <1318839214@qq.com>
2020-01-14 16:01:19 +0800

    1.14 16:00

--- react-lianxi/back-stage/src/pages/Goods/Add/Add.js
+++ react-lianxi/back-stage/src/pages/Goods/Add/Add.js
@@ -1,9 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import styles from './add.module.less'
 class AddGoods extends Component{
+  constructor(){
+    super()
+    this.state={
+      name:'红烧肉',
+      price:'199',
+      img:null,
+      desc:'好吃',
+      foodType:'热菜'
+    }
+  }
   render(){
+    let {name,price,desc,foodType,img}=this.state
     return(
       <div>
-        这是一个添加页面
+         name:<input className={styles.ipt} 
+         type='text'
+         value={name}
+         onChange={(e)=>{
+           this.setState({name:e.target.value})
+          console.log(e.target.value)
+         }}
+         /> 
+        <br/>
+        price:<input className={styles.ipt}
+          type='text'
+          
+         />
+        <br/>
+        foodType:<input className={styles.ipt}
+        type='text'
+        />
+        <br/>
+        desc:<input className={styles.ipt} 
+        type='text'
+        />
+        <br/>
+        img:<input className={styles.ipt} 
+        type='text'
+        />
       </div>
     )
   }
